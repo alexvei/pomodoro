83ae8e944e1f7cf2b78e93c38098ae903f07d38e
alexvei <vasilalexpanta@gmail.com>
2023-02-11 14:12:45 +0000

    Adds Sound

--- .main.py
+++ .main.py
@@ -1,15 +1,16 @@
 import time
-
+from playsound import playsound
 
 def pomodoro(a):
     for n in range(a):
         print(n)
         time.sleep(1)
-        if n == a  :
+        if n == a:
             break
     return "Pomodoro Session Complete!"
 
 
 a = int(input("Enter the session time: "))
 print(pomodoro(a))
+playsound('sound.wav')
 input("Press enter to exit")
