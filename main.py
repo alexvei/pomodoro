46b6964e22a79701c6cf82cb0411c6ba81bc7734
alexvei <vasilalexpanta@gmail.com>
2023-02-17 15:03:14 +0000

    Improvements on GUI.
    Now it displays the seconds in the window box instead of the console.

--- main.py
+++ main.py
@@ -2,79 +2,43 @@ import time
 # from playsound import playsound
 import tkinter as tk
 
-def pomodoro(mini):
-    """
-    This function implements a Pomodoro timer.
-    It takes the length of the Pomodoro session in minutes as input and
-    runs a timer that counts down the minutes and seconds in the session.
-    """
-    minu = int(mini)
-    # Convert minutes to seconds
-    min_to_sec = minu * 60
-    # Initialize the timer and minute counter
-    timer = 1
-    minuter = 0
 
-    # Run the timer until the desired time has passed
-    while timer != (min_to_sec + 1):
-        print(timer, end='\r')
-        thetime(timer)
-        time.sleep(1)
-        timer += 1
-        if timer == 60:
-            break
-    # Reset the timer to 0
+def window():
+    global body, minutes, minuted, minutes
+    body = tk.Tk()
+    minutes = tk.IntVar()
+    minuted = tk.IntVar()
+    body.title("Pomodoro")
+    body.geometry("300x300")
+    body.minsize(300, 300)
+    body.maxsize(300, 300)
+    label_one = tk.Label(body, text="Welcome to Pomodoro Timer!\n"
+                                    "Enter your session in minutes:")
+    label_one.pack()
+
+    label_two = tk.Label(body, textvariable=minutes)
+    label_two.pack()
+
+    label_three = tk.Label(body, textvariable=minuted)
+    label_three.pack()
+
+    entry_one = tk.Entry(body)
+    entry_one.bind("<Return>", lambda event: [minutes.set(entry_one.get()), timing()])
+    entry_one.pack()
+
+    button_one = tk.Button(body, text=f"Sumbit", command=lambda: [minutes.set(entry_one.get()), timing()])
+    button_one.pack()
+
+    body.mainloop()
+
+
+def timing():
     timer = 0
-    minuter += 1
-    
-   # Run the timer for the remaining minutes
-    while minuter != minu:
-        if timer == 60:
-            # Reset the timer to 0
-            timer = 0
-            minuter += 1
-        print(f'{minuter}:{timer}', end='\r')
-        time.sleep(1)
+    while timer != (minutes.get()*60+1):
+        minuted.set(timer)
+        body.update_idletasks()
         timer += 1
-    # Return a message indicating that the Pomodoro session is complete
-    
-    return "\nPomodoro Session Complete!"
+        time.sleep(1)
 
 
-def printInput():
-    setting_time = time_var.get()
-    pomodoro(setting_time)
-    time_set_label = tk.Label(root, text=f'Time set to: {setting_time}')
-    time_set_label.grid(row=1, column=0)
-    time_var.set("")
-
-def thetime(timi):
-    timi_label = tk.Label(root, text=timi)
-    timi_label.grid(row=2, column=0)
-
-
-# Play a sound to indicate that the session is complete
-# playsound('sound.wav')
-
-
-# Set the window name
-root = tk.Tk()
-root.title("Pomodoro")
-
-# Sizing
-root.geometry("300x300")
-root.minsize(300, 300)
-root.maxsize(300, 300)
-
-# Declare time variable
-time_var = tk.StringVar()
-
-time_label = tk.Label(root, text= 'Set the time in minutes:')
-time_entry = tk.Entry(root, textvariable = time_var)
-sub_btn = tk.Button(root,text = 'Set the time', command = printInput)
-
-time_label.grid(row=0, column=0)
-time_entry.grid(row=0, column=1)
-sub_btn.grid(row=1, column=1)
-
-root.mainloop()
+window()
